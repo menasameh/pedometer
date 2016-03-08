da11a6ae9d3ec4799c5f4664c5129a6d08248d05
Mina Sameh <minasamehlabib@gmail.com>
2016-03-08 16:45:03 +0200

    added GUI and steps accurate calculations

--- pedometer.ino
+++ pedometer.ino
@@ -8,6 +8,7 @@
 #include <LiquidCrystal.h>
 
 
+
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -17,9 +18,12 @@
 MPU6050 mpu;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-void displayOnLcd();
+void print_to_lCD();
 void getAccData();
-void runAlgo();
+void process_input(int i);
+int read_LCD_buttons();
+void runALgo();
+
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -39,30 +43,49 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 #define BUZZER_PIN 11
 #define READINGS_COUNT 200
 
+#define MOVING_SIZE 20
+#define SAMPLE_SIZE 50
+#define MAX_MIN_DIFF 2000
+#define NOW_PREV_DIFF 100
+
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+#define settings 0
+
+int st[3] , pos[3];
+int samples[3][SAMPLE_SIZE];
+int moving[3][MOVING_SIZE];
+int idx_sample[3];
+int max_min_diff[3];
+
 int readingsNum = 0, addr = 0, th=200;
 ////////////////////////////////////variables//////////////////////////////////
-int sample_cnt = 0;
-int sample_new[3] , sample_old[3] , max[3] , min[3];
-int steps = 0;
-int active_axis=0;
-int threshold;
-int precision=555;
+
+int state =0,Spage=0,Mpage=0,button_pressed=0;
+
 int timer=0;
 unsigned long timerStart=0;
-float distance=0;
 int sec_2=0;
 int steps_2=0;
-float height=1.87;
 float speed_2 , speed_sum=0 , speed_avg=0;
 float stride;
 float cal_2=0  , cal_sum=0;
-int weight = 1800;
 int cnt_2=0;
 
-int pos[3];
-int avg[3];
-int cnt=0;
 
+
+int steps;
+float height = 1.60;
+float weight = 80;
+float distance=0;
+float cal=0;
 
 
 
@@ -81,9 +104,7 @@ void setup() {
         Fastwire::setup(400, true);
     #endif
 
-  for(int i=0;i<3;i++){
-    avg[i]=0 ; max[i]=-1e9 ; min[i]=1e9;
-  }
+
 // set up the LCD's number of columns and rows:
 lcd.begin(16, 2);
 // Print a message to the LCD.
@@ -145,8 +166,16 @@ void loop() {
         // while() loop to immediately process the MPU data
         // .
         // .
-        if (timer>200)
-          displayOnLcd();
+
+        
+        
+    }
+    if (timer>200){
+          process_input(read_LCD_buttons());
+          print_to_lCD();
+        }
+        else if(timer == 200){
+          lcd.clear();
         }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -174,137 +203,112 @@ void loop() {
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
        if(timer < 200){
-        
         getAccData();
-        
        }
-       else if(timer == 200){
-          lcd.setCursor(0,0);
-          lcd.print("Steps :   ");
-//          unsigned long r = millis(); 
-//        lcd.setCursor(0,0);
-//        lcd.print("       "); 
-          runAlgo();
-//          lcd.setCursor(0,0);
-//        lcd.print(millis()-r);
-//        while(1);
-       }
-       else{
-         runAlgo();
+       else if(timer >= 200){
+          if(state){
+            runALgo();
+          }
        }
        timer++;
-       sec_2++;
-       if(sec_2*5 == 2000){
-        sec_2=0;
-        if(steps_2 >= 0 && steps_2 <= 2){
-          stride = height/5;
-          
-        }
-        else if(steps_2 >= 2 && steps_2 <= 3){
-           stride = height/4;
-        }
-         else if(steps_2 >= 3 && steps_2 <= 4){
-          stride = height/3;
-        }
-         else if(steps_2 >= 4 && steps_2 <= 5){
-          stride = height/2;
-        }
-         else if(steps_2 >= 5 && steps_2 <= 6){
-          stride = height/1.2;
-        }
-         else if(steps_2 >= 6 && steps_2 <= 8){
-          stride = height;
-        }
-        else{
-          stride = height*1.2;
-        }
-        distance+=stride*steps_2;
-        speed_2 = steps_2 * stride / 2;
-        if(steps_2){
-          cal_2 = speed_2 * weight / 400;
-        }
-        else{
-          cal_2 = weight / 1800;
-        }
-        cal_sum += cal_2;
-        speed_sum += speed_2;
-        speed_avg = speed_sum / cnt_2;
-        // lcd.setCursor(0,1);
-        // lcd.print(distance);
-        steps_2=0;
-        cnt_2++;
-       }
+//       sec_2++;
+//       if(sec_2*5 == 2000){
+//        sec_2=0;
+//        if(steps_2 >= 0 && steps_2 <= 2){
+//          stride = height/5;
+//          
+//        }
+//        else if(steps_2 >= 2 && steps_2 <= 3){
+//           stride = height/4;
+//        }
+//         else if(steps_2 >= 3 && steps_2 <= 4){
+//          stride = height/3;
+//        }
+//         else if(steps_2 >= 4 && steps_2 <= 5){
+//          stride = height/2;
+//        }
+//         else if(steps_2 >= 5 && steps_2 <= 6){
+//          stride = height/1.2;
+//        }
+//         else if(steps_2 >= 6 && steps_2 <= 8){
+//          stride = height;
+//        }
+//        else{
+//          stride = height*1.2;
+//        }
+//        distance+=stride*steps_2;
+//        speed_2 = steps_2 * stride / 2;
+//        if(steps_2){
+//          cal_2 = speed_2 * weight / 400;
+//        }
+//        else{
+//          cal_2 = weight / 1800;
+//        }
+//        cal_sum += cal_2;
+//        speed_sum += speed_2;
+//        speed_avg = speed_sum / cnt_2;
+//        // lcd.setCursor(0,1);
+//        // lcd.print(distance);
+//        steps_2=0;
+//        cnt_2++;
+//       }
     }
 }
 
-
-
-void runAlgo(){
-  //read acceleration data into 'pos' array
+void runALgo()
+{
   getAccData();
-  
-    cnt++;
-    for(int i=0;i<3;i++)
-      avg[i]+=pos[i];
-    if(cnt == 4){
-      cnt=0;
-      sample_cnt++;
-      // for(int i=0;i<3;i++){ // edited 
-      //   avg[i]/=4;
-      // }
-      if(sample_cnt == 1){
-        for(int i=0;i<3;i++)
-          sample_new[i] = sample_old[i] = avg[i];
-      }
-      else{
-        for(int i=0;i<3;i++){
-          sample_old[i] = sample_new[i];
-        }
-      }
-      for(int i=0;i<3;i++){
-        max[i] = max(max[i] , avg[i]);
-        min[i] = min(min[i] , avg[i]);
-      }
-      if(sample_cnt==50){
-        sample_cnt=0;
-        if(max[0]-min[0] > max[1]-min[1] && max[0]-min[0] > max[2]-min[2])
-          active_axis=0;
-        else if(max[1]-min[1] > max[2]-min[2] && max[1]-min[1] > max[0]-min[0])
-          active_axis=2;
-        else 
-          active_axis=3;
-        threshold = (max[active_axis] + min[active_axis])/2;
-        for(int i=0;i<3;i++){
-            max[i]=-1e9 ; min[i]=1e9;
-        }
-      }
-      if(abs(sample_new[0] - avg[0]) + abs(sample_new[1] - avg[1]) + abs(sample_new[2] - avg[2]) > precision){
-         for(int i=0;i<3;i++)
-           sample_new[i] = avg[i];
-      }  
-      
-      //Serial.print("%d , %d , %d         ,%d" ,avg[0] , avg[1] , avg[2] ,    threshold);   
-      Serial.print(avg[0]);
-      Serial.print(",");
-      Serial.print(avg[1]);
-      Serial.print(",");
-      Serial.print(avg[2]);
-      Serial.print(",");
-      Serial.print(threshold);
-      Serial.print(",");
-      Serial.println(steps);
-         
-      if(sample_new[active_axis] < threshold && threshold < sample_old[active_axis] && threshold > 800){ // edited 
-         if(millis() - timerStart > 12000){ // edited from 200
-           steps++;
-           steps_2++;
-           timerStart = millis();
-           
-         }
-      }
-      for(int i=0;i<3;i++)
-        avg[i] = 0;
+  for(int i = 0;i < 3;++i)
+    InternalRunAlgo(i);
+    
+  if(!(idx_sample[0] % 50))
+  {
+    int idx = 0;
+    for(int i = 1;i < 3;++i)
+    {
+      if(max_min_diff[i] > max_min_diff[idx])
+        idx = i;
     }
+    steps += st[idx];
+  }
+}
+
+void InternalRunAlgo(int axis)
+{
+  ++idx_sample[axis];
+  for(int i = 1;i < MOVING_SIZE;++i)
+    moving[axis][i - 1] = moving[axis][i];
+  
+  moving[axis][MOVING_SIZE - 1] = pos[axis];
+  if(abs(moving[axis][MOVING_SIZE - 1] - moving[axis][MOVING_SIZE - 2]) < NOW_PREV_DIFF)
+    moving[axis][MOVING_SIZE - 1] = moving[axis][MOVING_SIZE - 2];  
+  
+  long sum = 0;
+  for(int i = 0;i < MOVING_SIZE;++i)
+    sum += moving[axis][i];
+    
+  sum /= MOVING_SIZE;
+  for(int i = 1;i < SAMPLE_SIZE;++i)
+    samples[axis][i - 1] = samples[axis][i];
+  
+  samples[axis][SAMPLE_SIZE - 1] = sum;
+  if(!(idx_sample[axis] % 50))
+  {
+    st[axis] = 0;
+    int ma = -1e9 , mi = 1e9;
+    for(int i = 0;i < SAMPLE_SIZE;++i)
+    {
+      ma = max(ma , samples[axis][i]);
+      mi = min(mi , samples[axis][i]);
+    }
+    max_min_diff[axis] = ma - mi;
+    int threshold = (ma + mi) / 2;
+    for(int i = 1;i < SAMPLE_SIZE;++i)
+    {
+      if(samples[axis][i - 1] >= threshold && samples[axis][i] <= threshold && ma - mi > MAX_MIN_DIFF)
+        ++st[axis];
+    }
+  }
 }
 
 
@@ -326,9 +330,138 @@ void beep(){
   digitalWrite(11, LOW);
 }
 
-void displayOnLcd(){
-        /////////////////////////////////////display on screen /////////////////////////////////////
-  lcd.setCursor(7,0);
-  lcd.print(steps);
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0);      // read the value from the sensor 
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+ Serial.println(adc_key_in);
+  if (adc_key_in > 1000){
+     button_pressed =15;
+     return btnNONE;
+  }
+ 
+  if (abs(button_pressed-adc_key_in)<=10) return btnNONE;
+
+  if (adc_key_in < 50){   
+    button_pressed = adc_key_in; 
+    return btnRIGHT;
+    }  
+  if (adc_key_in < 150){  
+    button_pressed = adc_key_in; 
+    return btnUP;
+    } 
+  if (adc_key_in < 300){  
+    button_pressed = adc_key_in; 
+    return btnDOWN;
+    } 
+  if (adc_key_in < 450){  
+    button_pressed = adc_key_in; 
+    return btnLEFT;
+    } 
+  if (adc_key_in < 850){
+    button_pressed = adc_key_in; 
+    return btnSELECT;
+    }     
+  return btnNONE;  // when all others fail, return this...
 }
+
+void print_to_lCD()
+{
+  if(state == settings ){
+    if(Spage == 0){
+      lcd.setCursor(3,0);
+      lcd.print(" Settings  ");
+      lcd.setCursor(0,1);
+      lcd.print("height : ");
+      lcd.print(height);
+    }
+    else if(Spage == 1){
+      lcd.setCursor(3,0);
+      lcd.print(" Settings  ");
+      lcd.setCursor(0,1);
+      lcd.print("weight : ");
+      lcd.print(weight);
+    }
+  }
+  else{
+    if(Mpage == 0){
+      lcd.setCursor(3,0);
+      lcd.print("Measurement");
+      lcd.setCursor(0,1);
+      lcd.print("steps : ");
+      lcd.print(steps);
+    }
+    else if(Mpage == 1){
+      lcd.setCursor(3,0);
+      lcd.print("Measurement");
+      lcd.setCursor(0,1);
+      lcd.print("distance : ");
+      lcd.print(distance);
+    }
+    else if(Mpage == 2){
+      lcd.setCursor(3,0);
+      lcd.print("Measurement");
+      lcd.setCursor(0,1);
+      lcd.print("cals : ");
+      lcd.print(cal);
+    }
+  }
+}
+
+void clear_lcd(){
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void process_input(int input){
+  switch(input){
+    case btnRIGHT:
+      if(state == settings){
+        Spage = (Spage+1)%2;
+      }
+      else{
+        Mpage = (Mpage+1)%3;
+      }
+      clear_lcd();
+      break;
+    case btnUP:
+      if(state == settings){
+        if(Spage == 0){
+          height = min(height+0.1, 2.50);
+        }
+        else{
+          weight = min(weight+5, 200);
+        }
+      }
+      break;
+    case btnDOWN:
+      if(state == settings){
+        if(Spage == 0){
+          height = max(height-0.1, 0.50);
+        }
+        else{
+          weight = max(weight-5, 30);
+        }
+        lcd.setCursor(9, 1);
+        lcd.print("    ");
+      }
+      break;
+    case btnLEFT:
+      if(state == settings){
+        Spage = (Spage-1+2)%2;
+      }
+      else{
+        Mpage = (Mpage-1+3)%3;
+      }
+      clear_lcd();
+      break;
+    case btnSELECT:
+      state =1;
+//      state = (state == settings?1:settings);
+      lcd.clear();
+      break;
+  }
+}
+
 
